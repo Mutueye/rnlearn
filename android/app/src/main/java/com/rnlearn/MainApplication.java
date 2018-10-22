6185715b2751c3a10bae7f92d9c59c9335f58e37
mutueye <mutuyueniao@163.com>
2018-10-22 18:32:42 +0800

    fix

--- android/app/src/main/java/com/rnlearn/MainApplication.java
+++ android/app/src/main/java/com/rnlearn/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.reactlibrary.RNTooltipsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,9 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RNTooltipsPackage(),
-            new VectorIconsPackage()
+          new VectorIconsPackage()
       );
     }
 
